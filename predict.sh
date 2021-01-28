d3bcfcb79ad3c40f37d2f99df4dcc80f9426e92a
zcx <zcx@umich.edu>
2021-01-28 11:18:13 -0500

    include blast baseline

--- predict.sh
+++ predict.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 FILE=`readlink -e $0`
 rootdir=`dirname $FILE`
-
+cd $rootdir/prediction
 for species in `cat $rootdir/input/list`;do
     echo "predict GO for species $species"
-    $rootdir/bin/predict_naive.py $rootdir/input/target.$species.fasta $rootdir/prediction/naive_1_${species}_go.txt
+    $rootdir/bin/predict_naive.py $rootdir/input/target.$species.fasta naive_1_${species}_go.txt
+    $rootdir/bin/predict_blast.py $rootdir/input/target.$species.fasta _${species}_go.txt
 done
